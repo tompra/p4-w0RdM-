514cf0f375354e88e2fb964d8a8f344c826a3927
tompra <tomprayon@gmail.com>
2024-06-05 14:35:21 +0200

    render title logo in terminal

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import colorama
 from colorama import Fore, Style, Back
 import pyfiglet
 
+def print_logo():
+    colorama.init()
+    logo = pyfiglet.figlet_format('Password Manager')
+    print(Fore.LIGHTCYAN_EX + logo + Style.RESET_ALL)
+    
+print_logo()
