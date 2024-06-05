d57d7b72c8055769a4d1a6f9a0cb0b981c2831df
tompra <tomprayon@gmail.com>
2024-06-05 14:54:48 +0200

    render menu and welcoming text

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ import pyfiglet
 
 def print_logo():
     colorama.init()
-    logo = pyfiglet.figlet_format('Password Manager')
+    logo = pyfiglet.figlet_format('Password Manager', font='larry3d')
     print(Fore.LIGHTCYAN_EX + logo + Style.RESET_ALL)
     
 print_logo()
+
+def welcome_text():
+    print('Protect yourself with secure passwords, passkeys and sensitive information.\n')
+
+welcome_text()
+
+def menu():
+    print('1. Register')
+    print('2. Login')
+    print('3. Exit')
+    return input('Enter your choice: ')
+
+menu()
